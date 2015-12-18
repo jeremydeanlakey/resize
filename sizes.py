f52147166e97dfaf80c02e5d185a57872c4b9287
Jeremy Dean Lakey <jeremy.lakey@gmail.com>
2015-12-17 18:06:06 -0700

    add dynamic calculation of android sizes

--- sizes.py
+++ sizes.py
@@ -1,5 +1,11 @@
 
 
+MDPI =  "drawable-mdpi"
+HDPI =  "drawable-hdpi"
+XHDPI =  "drawable-xhdpi"
+XXHDPI =  "drawable-xxhdpi"
+XXXHDPI =  "drawable-xxxhdpi"
+
 ANDROID_20DP = [
 ((24, 24), "drawable-mdpi"),
 ((36, 36), "drawable-hdpi"),
@@ -19,3 +25,12 @@ ANDROID_40DP = [
 
 
 ANDROID_LAUNCHER = ANDROID_40DP + [((512, 512), "google_play_store")]
+
+
+def calc_android_sizes(dp):
+    return [((int(dp*1.2), int(dp*1.2)), MDPI),
+            ((int(dp*1.8), int(dp*1.8)), HDPI),
+            ((int(dp*2.4), int(dp*2.4)), XHDPI),
+            ((int(dp*3.6), int(dp*3.6)), XXHDPI),
+            ((int(dp*4.8), int(dp*4.8)), XXXHDPI)]
+
