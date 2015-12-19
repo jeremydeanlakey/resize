065f4c7f63e7036166c6431c54be0ffd0e910d0a
Jeremy Dean Lakey <jeremy.lakey@gmail.com>
2015-12-18 18:12:59 -0700

    simplify sizes.py

--- sizes.py
+++ sizes.py
@@ -1,31 +1,8 @@
-
-
 MDPI =  "drawable-mdpi"
 HDPI =  "drawable-hdpi"
 XHDPI =  "drawable-xhdpi"
 XXHDPI =  "drawable-xxhdpi"
 XXXHDPI =  "drawable-xxxhdpi"
-
-ANDROID_20DP = [
-((24, 24), "drawable-mdpi"),
-((36, 36), "drawable-hdpi"),
-((48, 48), "drawable-xhdpi"),
-((72, 72), "drawable-xxhdpi"),
-((96, 96), "drawable-xxxhdpi"),
-]
-
-
-ANDROID_40DP = [
-((48, 48), "drawable-mdpi"),
-((72, 72), "drawable-hdpi"),
-((96, 96), "drawable-xhdpi"),
-((144, 144), "drawable-xxhdpi"),
-((192, 192), "drawable-xxxhdpi"),
-]
-
-
-ANDROID_LAUNCHER = ANDROID_40DP + [((512, 512), "google_play_store")]
-
 
 def calc_android_sizes(dp):
     return [((int(dp*1.2), int(dp*1.2)), MDPI),
@@ -33,4 +10,10 @@ def calc_android_sizes(dp):
             ((int(dp*2.4), int(dp*2.4)), XHDPI),
             ((int(dp*3.6), int(dp*3.6)), XXHDPI),
             ((int(dp*4.8), int(dp*4.8)), XXXHDPI)]
+
+ANDROID_20DP = calc_android_sizes(20)
+ANDROID_40DP = calc_android_sizes(40)
+ANDROID_LAUNCHER = ANDROID_40DP + [((512, 512), "google_play_store")]
+
+
 
