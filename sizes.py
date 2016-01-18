7f3693df2cff4664bc98338bbe9fab5b9c2c6788
Jeremy Dean Lakey <jeremy.lakey@gmail.com>
2016-01-18 07:29:40 -0700

    add list of ios sizes

--- sizes.py
+++ sizes.py
@@ -16,4 +16,14 @@ ANDROID_40DP = calc_android_sizes(40)
 ANDROID_LAUNCHER = ANDROID_40DP + [((512, 512), "google_play_store")]
 
 
+# source: http://makeappicon.com/ios7icon
+IOS_ICON_SMALL = ((29, 29), "Icon-Small")
+IOS_ICON_SMALL_2x = ((58, 58), "Icon-Small@2x")
+IOS_ICON_40 = ((40, 40), "Icon-40")
+IOS_ICON_40_2x = ((80, 80), "Icon-40@2x")
+IOS_ICON_60_2x = ((120, 120), "Icon-60@2x")
+IOS_ICON_76 = ((76, 76), "Icon-76")
+IOS_ICON_76_2x = ((152, 152), "Icon-76@2x")
+IOS_ITUNES = ((512, 512), "iTunesArtwork")
+IOS_ITUNES_2x = ((1024, 1024), "iTunesArtwork@2x")
 
